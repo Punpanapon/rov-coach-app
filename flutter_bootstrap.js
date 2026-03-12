1fdc13ca1bdee408bbc798e5facf0f3107b62c44
Panapon <thpunpun@gmail.com>
2026-03-12 23:37:33 +0700

    Epic draft: view mode toggle, pulsing turn indicator, lock-in animations, hover glow

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2675591468"
+    serviceWorkerVersion: "3538237625"
   }
 });
